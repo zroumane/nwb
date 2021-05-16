d3e2fc907a26a7bd7ce745a4717ea9de6cf43864
zephyr <53237042+zroumane@users.noreply.github.com>
2021-05-17 00:04:33 +0200

    update zephyr 17/08

--- assets/js/weapon.js
+++ assets/js/weapon.js
@@ -2,15 +2,31 @@ import "../css/weapon.scss";
 import { $q, $qa } from "./global";
 
 const $weaponSelect = $q("#weaponSelect");
-const $createWeaponForm = $q("#createWeapon");
+const $weaponForm = $q("#weaponForm");
+const $skillSection = $q("#skillSection");
+const $skillForm = $q("#skillForm");
 
-function getWeapon() {
+/* Fonction de Request adaptative */
+const request = async (url, method, body, callback) => {
+	let args = { headers: { "content-Type": "application/json" }, method: method };
+	if (body != undefined) args.body = JSON.stringify(body);
+	let response = await fetch(url, args);
+	if (200 <= response.status && response.status < 300)
+		window.setTimeout(() => {
+			callback();
+		}, 500);
+	else console.log("Erreur");
+};
+
+/* Fonction de mise a jour de la liste d'arme */
+const getWeapon = () => {
 	fetch("/api/weapons")
 		.then((response) => response.json())
 		.then((data) => {
 			Array.from($q("#weaponSelect").children)
 				.filter((c) => c.value != 0)
 				.forEach((c) => c.remove());
+			console.log(data["hydra:member"]);
 			window.weapons = data["hydra:member"];
 			window.weapons.forEach((weapon) => {
 				var $weaponOption = document.createElement("option");
@@ -19,68 +35,68 @@ function getWeapon() {
 				$weaponSelect.appendChild($weaponOption);
 			});
 			Array.from($weaponSelect.children).filter((c) => c.value == 0)[0].selected = true;
-			fillCreateForm(["", "", ""]);
+			fillCreateForm(undefined);
 		});
-}
+};
+getWeapon();
 
+/* Au changement d'arme */
 $weaponSelect.addEventListener("change", () => {
 	var selectedValue = $weaponSelect.value;
-	if (selectedValue == 0) var data = ["", "", ""];
-	else {
-		var selectedWeaponData = window.weapons.filter((w) => w.id == selectedValue)[0];
-		var data = [selectedWeaponData.weaponKey, selectedWeaponData.branch[0], selectedWeaponData.branch[1]];
-	}
+	if (selectedValue == 0) return fillCreateForm(undefined);
+	var selectedWeaponData = window.weapons.filter((w) => w.id == selectedValue)[0];
+	var data = [selectedWeaponData.weaponKey, selectedWeaponData.branch[0], selectedWeaponData.branch[1]];
 	fillCreateForm(data);
 });
 
-function fillCreateForm(data) {
-	$createWeaponForm.querySelector('input[data-type="wkey"]').value = data[0];
-	$createWeaponForm.querySelector('input[data-type="b1key"]').value = data[1];
-	$createWeaponForm.querySelector('input[data-type="b2key"]').value = data[2];
-	$q("#branchName-1").innerText = data[1];
-	$q("#branchName-2").innerText = data[2];
-}
+/* Mise a jour du contenu en fonction de l'arme selectionnée */
+const fillCreateForm = (data) => {
+	if (data === undefined) {
+		$skillSection.classList.add("isHidden");
+		$weaponForm.querySelector('input[data-type="wkey"]').value = "";
+		$weaponForm.querySelector('input[data-type="b1key"]').value = "";
+		$weaponForm.querySelector('input[data-type="b2key"]').value = "";
+	} else {
+		$skillForm.classList.add("isHidden");
+		$skillSection.classList.remove("isHidden");
+		$weaponForm.querySelector('input[data-type="wkey"]').value = data[0];
+		$weaponForm.querySelector('input[data-type="b1key"]').value = data[1];
+		$weaponForm.querySelector('input[data-type="b2key"]').value = data[2];
+		$q("#branchName-1").innerText = data[1];
+		$q("#branchName-2").innerText = data[2];
+		// TODO : remplir les skills
+	}
+};
 
-$qa(".weaponAction").forEach((btn) => {
-	btn.addEventListener("click", () => {
-		var fetchObject = {
-			headers: {
-				"content-Type": "application/json",
-			},
-		};
-
-		var selectedValue = "" ? $weaponSelect.value == 0 : "/" + $weaponSelect.value;
-
-		if (btn.getAttribute("data-type") == "delete") {
-			if (selectedValue == "") return;
-			else fetchObject.method = "DELETE";
-		} else {
-			if (btn.getAttribute("data-type") == "submit")
-				if ($weaponSelect.value != 0) fetchObject.method = "PUT";
-				else fetchObject.method = "POST";
-
-			fetchObject.body = JSON.stringify({
-				weaponKey: $createWeaponForm.querySelector('input[data-type="wkey"]').value,
-				branch: [
-					$createWeaponForm.querySelector('input[data-type="b1key"]').value,
-					$createWeaponForm.querySelector('input[data-type="b2key"]').value,
-				],
-			});
-		}
-
-		fetch(`/api/weapons${selectedValue}`, fetchObject).then((response) => {
-			if (200 <= response.status && response.status < 300) getWeapon();
-		});
-	});
+/* Supprimer une arme */
+const $deleteWeaponBtn = $q('.weaponAction[data-type="delete"]');
+$deleteWeaponBtn.addEventListener("click", () => {
+	if ($weaponSelect.value == 0) return;
+	request(`/api/weapons/${$weaponSelect.value}`, "delete", undefined, getWeapon);
 });
 
-getWeapon();
+/* Ajouter / Mettre a jour une arme */
+const $sendWeaponBtn = $q('.weaponAction[data-type="submit"]');
+$sendWeaponBtn.addEventListener("click", () => {
+	var method,
+		body,
+		selectedValue = $weaponSelect.value == 0 ? "" : "/" + $weaponSelect.value;
+	if ($weaponSelect.value == 0) method = "POST";
+	else method = "PUT";
+	body = {
+		weaponKey: $weaponForm.querySelector('input[data-type="wkey"]').value,
+		branch: [$weaponForm.querySelector('input[data-type="b1key"]').value, $weaponForm.querySelector('input[data-type="b2key"]').value],
+	};
+	request(`/api/weapons${selectedValue}`, method, body, getWeapon);
+});
 
+/* Au click d'un contenaire skill */
 $qa(".skill-container").forEach((skillContainer) => {
 	skillContainer.addEventListener("click", () => {
 		var data = skillContainer.id.split("-");
 		var side = data[1];
 		var row = data[2];
 		var col = data[3];
+		$skillForm.classList.remove("isHidden");
 	});
 });
