64afaf4af6da79449992adbee84b96d9022bed89
Shokox <77963969+Shokox@users.noreply.github.com>
2021-05-17 22:41:17 +0200

    Début edit profil

--- src/Controller/ProfileController.php
+++ src/Controller/ProfileController.php
@@ -3,7 +3,9 @@
 namespace App\Controller;
 
 use App\Entity\User;
+use App\Form\EditProfileType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -33,5 +35,27 @@ class ProfileController extends AbstractController
 				"user" => $user,
 			]);
 		}
+	}
+
+	/**
+	 * @Route("/profile/edit", name="profile_edit")
+	 */
+	public function edit(Request $request): Response
+	{
+		$user = $this->getUser();
+		$form = $this->createForm(EditProfileType::class, $user);
+		$form->handleRequest($request);
+
+		if ($form->isSubmitted() && $form->isValid()) {
+			$entityManager = $this->getDoctrine()->getManager();
+			$entityManager->persist($user);
+			$entityManager->flush();
+
+			return $this->redirectToRoute("profile");
+		}
+
+		return $this->render("profile/edit.html.twig", [
+			"form" => $form->createView(),
+		]);
 	}
 }
