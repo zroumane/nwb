220e84bd8ec7b72677ba8c4b26037b7bc41e218f
= <=>
2021-07-04 19:05:22 +0200

    fix navbar

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,5 +1,6 @@
 import "../css/app.scss";
 import "bootstrap/js/dist/dropdown";
+import "bootstrap/js/dist/collapse";
 
 const $q = document.querySelector.bind(document);
 const $qa = document.querySelectorAll.bind(document);
