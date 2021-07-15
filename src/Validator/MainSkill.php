08e3b41f21d8c2e9ea0b6b46ad087945030ba94d
zroumane <zroumane@github.com>
2021-07-16 00:45:15 +0200

    up main skill validation

--- src/Validator/MainSkill.php
+++ src/Validator/MainSkill.php
@@ -9,6 +9,8 @@ use Symfony\Component\Validator\Constraint;
  */
 class MainSkill extends Constraint
 {
+  public $messageNotFound = "Some mainSkill(s) not exist(s).";
   public $messageLenght = "There is the wrong mainSkills array length.";
   public $messageNotActive = "Some mainSkill(s) are not activeSkill(s).";
+  public $messageNotAbility = "Some mainSkill(s) are not abilities.";
 }
--- src/Validator/MainSkillValidator.php
+++ src/Validator/MainSkillValidator.php
@@ -3,25 +3,41 @@
 namespace App\Validator;
 
 use Symfony\Component\Validator\Constraint;
+use ApiPlatform\Core\Api\IriConverterInterface;
 use Symfony\Component\Validator\ConstraintValidator;
 use Symfony\Component\Validator\Exception\UnexpectedTypeException;
 use Symfony\Component\Validator\Exception\UnexpectedValueException;
 
+
 class MainSkillValidator extends ConstraintValidator
 {
+
+  private $iriConverter;
+
+  public function __construct(IriConverterInterface $iriConverter)
+  {
+    $this->iriConverter = $iriConverter;
+  }
 
   public function validate($mainSkills, Constraint $constraint)
   {
     $activeSkill = $this->context->getObject()->getActiveSkills();
-    
-    foreach($mainSkills as $weaponIndex => $weaponMainSkill) {
+    foreach($mainSkills as $weaponIndex => $weaponMainSkills) {
       $weaponActiveSkill = $activeSkill[$weaponIndex];
-      if(count($weaponMainSkill) != 3){
+      if(count($weaponMainSkills) != 3){
         $this->context->buildViolation($constraint->messageLenght)->addViolation();
       }
-      foreach ($weaponMainSkill as $key => $mainSkill) {
+      foreach ($weaponMainSkills as $key => $mainSkill) {
         if($mainSkill == null){
           continue;
+        }
+        try {
+          $mainSkillItem = $this->iriConverter->getItemFromIri($mainSkill);
+          if($mainSkillItem->type != 1){
+            $this->context->buildViolation($constraint->messageNotAbility)->addViolation();
+          }
+        } catch (\Throwable $th) {
+          $this->context->buildViolation($constraint->messageNotFound)->addViolation();
         }
         if(!in_array($mainSkill, $weaponActiveSkill)){
           $this->context->buildViolation($constraint->messageNotActive)->addViolation();
