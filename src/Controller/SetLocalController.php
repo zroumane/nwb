40bc3c0dc58a9b1e7f4670312e7092ecd78f0728
zroumane <zroumane@github.com>
2021-08-14 20:28:12 +0200

    fix: set locale

--- src/Controller/SetLocalController.php
+++ src/Controller/SetLocalController.php
@@ -17,15 +17,12 @@ class SetLocalController
    */
   public function index(Request $request, $locale = null)
   {
-    // $locale_array = ['de','en','es','fr','it','pl','pt'];
     $locale_array = ["en", "fr"];
 
     if (in_array($locale, $locale_array)) {
-      // On enregistre la langue en session
       $request->getSession()->set("_locale", $locale);
     }
-
-    // on tente de rediriger vers la page d'origine
+    
     $url = $request->headers->get("referer");
     if (empty($url)) {
       $url = $this->container->get("router")->generate("index");
--- src/EventSubscriber/RequestSubscriber.php
+++ src/EventSubscriber/RequestSubscriber.php
@@ -12,34 +12,27 @@ class RequestSubscriber implements EventSubscriberInterface
   public static function getSubscribedEvents()
   {
     return [
-      // must be registered before (i.e. with a higher priority than) the default Locale listener
       KernelEvents::REQUEST => [["onKernelRequest", 20]],
     ];
   }
 
   public function onKernelRequest(RequestEvent $event)
-  {
+  {    
     $request = $event->getRequest();
     $session = $request->getSession();
 
     if (!$request->hasPreviousSession()) {
-      if (array_key_exists("HTTP_ACCEPT_LANGUAGE", $_SERVER)) {
-        $locale = substr($_SERVER["HTTP_ACCEPT_LANGUAGE"], 0, 2);
+      return;
+    }
 
-        // $locale_array = ['de','en','es','fr','it','pl','pt'];
-        $locale_array = ["en", "fr"];
-
-        $locale = in_array($locale, $locale_array) ? $locale : $request->getDefaultLocale();
-        $request->setLocale($locale);
-      }
-    }else{ 
-      $request->setLocale($session->get("_locale", $request->getDefaultLocale()));
+    if ($locale = $request->attributes->get('_locale')) {
+      $request->getSession()->set('_locale', $locale);
+    } else {
+      $request->setLocale($request->getSession()->get('_locale',$request->getDefaultLocale()));
     }
 
     if(!$session->get('views')){
       $session->set('views', []);
     }
-    
   }
-
 }
